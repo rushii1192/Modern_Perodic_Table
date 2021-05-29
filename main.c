07894243a99e8b413844e5bb877e86ea324a070b
Rushi <68375572+rushii1192@users.noreply.github.com>
2021-05-29 15:59:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "elements.h"
-
-int i;
+#include "header_files/elements.h"
 
 void search();
-
+void again();
 void clrscr() {
     system("@cls||clear");
 }
-
+void info2(){
+	
+}
 void newScreen() {
 	clrscr();
 	int width = 132;
@@ -21,6 +21,78 @@ void newScreen() {
 	printf("%*.*s%s\n", pad, pad, " ", str);
 	for(int i=0; i<10; i++){
 		printf("\n");
+	}
+}
+void info(char element_name[20],char element_symbol[4],int atomic_number,float atomic_weight){
+	struct element user_element;
+	if(element_name!=""){
+		user_element = element_searcher(element_name,"",0,0);
+	}
+	if(element_symbol!=""){
+		user_element = element_searcher("",element_symbol,0,0);
+	}
+	if(atomic_number!=0){
+		user_element = element_searcher("","",atomic_number,0);
+	}
+	if (atomic_weight!=0)
+	{
+		user_element = element_searcher("","",0,atomic_weight);
+	}
+	printf("\033[36m\tELEMENT : %s\033[0m\n",user_element.name);
+	printf("\033[36m\tSYMBOL : %s\033[0m\n",user_element.symbol);
+	printf("\033[36m\tATOMIC NUMBER : %d\033[0m\n",user_element.atomic_number);
+	printf("\033[36m\tATOMIC WEIGHT : %f\033[0m\n",user_element.atomic_weight);
+	printf("\033[36m\tELECTRONIC CONFIGURATION : %s\033[0m\n",user_element.electronic_configuration);
+	printf("\033[36m\tHISTORY : %s\033[0m\n",user_element.history);
+	printf("\033[36m\tMELTING POINT : %f\033[0m\n",user_element.melting_point);
+	printf("\033[36m\tBOILING POINT : %f\033[0m\n",user_element.boiling_point);
+	printf("\033[36m\tIONIC RADIUS : %f\033[0m\n",user_element.ionic_radius);
+	printf("\033[36m\tISOTOPES : %d\033[0m\n",user_element.isotopes);
+	printf("\033[36m\tELECTRONEGATIVITY : %f\033[0m\n",user_element.electronegativity);
+	printf("\033[36m\tELEMENT COLOUR : %s\033[0m\n",user_element.color);
+	printf("\033[36m\tPOSITION : ELEMENT IS IN  %d GROUP and %d PERIOD\033[0m\n",user_element.group,user_element.period);
+	printf("\033[36m\tCONDUCTIVITY : %s\033[0m\n",user_element.conductivity);
+	printf("\033[36m\tLUSTER : %s\033[0m\n",user_element.luster);
+	printf("\033[36m\tSTATE OF MATTER : %s\033[0m\n",user_element.phase);
+	printf("\033[36m\tDENSITY : %f\033[0m\n",user_element.density);
+	printf("\033[36m\tUses : %s\033[0m\n",user_element.uses);
+	again();	
+}
+void search() {
+	newScreen();
+	char ele[20];
+	char sym[4];
+	int atn;
+	float atwt;
+	printf("\033[32mSEARCH BY :- \033[0m\n\n");
+	printf("\033[33m\t1 -> ELEMENT\033[0m\n");
+	printf("\033[33m\t2 -> SYMBOL\033[0m\n");
+	printf("\033[33m\t3 -> ATOMIC NUMBER\033[0m\n");
+	printf("\033[33m\t4 -> ATOMIC WEIGHT\033[0m\n\n\n\n");
+	int choice2;
+	printf("\033[32mYOUR CHOICE : \033[0m");
+	scanf("%d",&choice2);
+	if (choice2==1){
+		printf("\033[32mENTER THE ELEMENT'S NAME : \033[0m");
+		scanf("%s",&ele);
+		info(ele,"",0,0);
+	} else if (choice2==2){
+		
+		printf("\033[32mENTER THE ELEMENT'S SYMBOL : \033[0m");
+		scanf("%s",&sym);
+		info("",sym,0,0);
+	} else if (choice2==3){
+		
+		printf("\033[32mENTER THE ELEMENT'S ATOMIC NUMBER : \033[0m");
+		scanf("%d",&atn);
+		info("","",atn,0);
+	} else if (choice2==4){
+		
+		printf("\033[32mENTER THE ELEMENT'S ATOMIC WEIGHT : \033[0m");
+		scanf("%f",&atwt);
+		info("","",0,atwt);
+	} else {
+		search();
 	}
 }
 
@@ -48,161 +120,13 @@ void again() {
 	printf("\n\n\n\n\n\n\n\n\n\n");
 }
 
-void info1() {
-	newScreen();
-	char ele[20];
-	printf("\033[32mENTER THE ELEMENT'S NAME : \033[0m");
-	scanf("%s",ele);
-	for (i=0; i<118; i++) {
-		if (strcmp(atom[i].name,ele)==0){
-			printf("\033[36m\tELEMENT : %s\033[0m\n",atom[i].name);
-			printf("\033[36m\tSYMBOL : %s\033[0m\n",atom[i].symbol);
-			printf("\033[36m\tATOMIC NUMBER : %d\033[0m\n",atom[i].atomicnum);
-			printf("\033[36m\tATOMIC WEIGHT : %f\033[0m\n",atom[i].atomicwt);
-			printf("\033[36m\tELECTRONIC CONFIGURATION : %s\033[0m\n",atom[i].electronic_configuration);
-			printf("\033[36m\tHISTORY : %s\033[0m\n",atom[i].history);
-			printf("\033[36m\tMELTING POINT : %f\033[0m\n",atom[i].melting_point);
-			printf("\033[36m\tBOILING POINT : %f\033[0m\n",atom[i].boiling_point);
-			printf("\033[36m\tIONIC RADIUS : %f\033[0m\n",atom[i].ionic_radius);
-			printf("\033[36m\tISOTOPES : %d\033[0m\n",atom[i].isotopes);
-			printf("\033[36m\tELECTRONEGATIVITY : %f\033[0m\n",atom[i].electronegativity);
-			printf("\033[36m\tELEMENT COLOUR : %s\033[0m\n",atom[i].color);
-			printf("\033[36m\tPOSITION : ELEMENT IS IN  %d GROUP and %d PERIOD\033[0m\n",atom[i].group,atom[i].period);
-			printf("\033[36m\tCONDUCTIVITY : %s\033[0m\n",atom[i].conductivity);
-			printf("\033[36m\tLUSTER : %s\033[0m\n",atom[i].luster);
-			printf("\033[36m\tSTATE OF MATTER : %s\033[0m\n",atom[i].phase);
-			printf("\033[36m\tDENSITY : %f\033[0m\n",atom[i].density);
-			printf("\033[36m\tUses : %s\033[0m\n",atom[i].uses);
-			break;
-		}
-	}
-	again();
-}
-
-void info2() {
-	newScreen();
-	char sym[1];
-	printf("\033[32mENTER THE ELEMENT'S SYMBOL : \033[0m");
-	scanf("%s",sym);
-	for (i=0; i<118; i++) {
-		if (strcmp(atom[i].symbol,sym)==0){
-			printf("\033[36m\tELEMENT : %s\033[0m\n",atom[i].name);
-			printf("\033[36m\tSYMBOL : %s\033[0m\n",atom[i].symbol);
-			printf("\033[36m\tATOMIC NUMBER : %d\033[0m\n",atom[i].atomicnum);
-			printf("\033[36m\tATOMIC WEIGHT : %f\033[0m\n",atom[i].atomicwt);
-			printf("\033[36m\tELECTRONIC CONFIGURATION : %s\033[0m\n",atom[i].electronic_configuration);
-			printf("\033[36m\tHISTORY : %s\033[0m\n",atom[i].history);
-			printf("\033[36m\tMELTING POINT : %f\033[0m\n",atom[i].melting_point);
-			printf("\033[36m\tBOILING POINT : %f\033[0m\n",atom[i].boiling_point);
-			printf("\033[36m\tIONIC RADIUS : %f\033[0m\n",atom[i].ionic_radius);
-			printf("\033[36m\tISOTOPES : %d\033[0m\n",atom[i].isotopes);
-			printf("\033[36m\tELECTRONEGATIVITY : %f\033[0m\n",atom[i].electronegativity);
-			printf("\033[36m\tELEMENT COLOUR : %s\033[0m\n",atom[i].color);
-			printf("\033[36m\tPOSITION : ELEMENT IS IN  %d GROUP and %d PERIOD\033[0m\n",atom[i].group,atom[i].period);
-			printf("\033[36m\tCONDUCTIVITY : %s\033[0m\n",atom[i].conductivity);
-			printf("\033[36m\tLUSTER : %s\033[0m\n",atom[i].luster);
-			printf("\033[36m\tSTATE OF MATTER : %s\033[0m\n",atom[i].phase);
-			printf("\033[36m\tDENSITY : %f\033[0m\n",atom[i].density);
-			printf("\033[36m\tUses : %s\033[0m\n",atom[i].uses);
-			break;
-		}
-	}
-	again();
-}
-
-void info3() {
-	newScreen();
-	int atn;
-	printf("\033[32mENTER THE ELEMENT'S ATOMIC NUMBER : \033[0m");
-	scanf("%d",&atn);
-	for (i=0; i<118; i++) {
-		if (atom[i].atomicnum==atn){
-			printf("\033[36m\tELEMENT : %s\033[0m\n",atom[i].name);
-			printf("\033[36m\tSYMBOL : %s\033[0m\n",atom[i].symbol);
-			printf("\033[36m\tATOMIC NUMBER : %d\033[0m\n",atom[i].atomicnum);
-			printf("\033[36m\tATOMIC WEIGHT : %f\033[0m\n",atom[i].atomicwt);
-			printf("\033[36m\tELECTRONIC CONFIGURATION : %s\033[0m\n",atom[i].electronic_configuration);
-			printf("\033[36m\tHISTORY : %s\033[0m\n",atom[i].history);
-			printf("\033[36m\tMELTING POINT : %f\033[0m\n",atom[i].melting_point);
-			printf("\033[36m\tBOILING POINT : %f\033[0m\n",atom[i].boiling_point);
-			printf("\033[36m\tIONIC RADIUS : %f\033[0m\n",atom[i].ionic_radius);
-			printf("\033[36m\tISOTOPES : %d\033[0m\n",atom[i].isotopes);
-			printf("\033[36m\tELECTRONEGATIVITY : %f\033[0m\n",atom[i].electronegativity);
-			printf("\033[36m\tELEMENT COLOUR : %s\033[0m\n",atom[i].color);
-			printf("\033[36m\tPOSITION : ELEMENT IS IN  %d GROUP and %d PERIOD\033[0m\n",atom[i].group,atom[i].period);
-			printf("\033[36m\tCONDUCTIVITY : %s\033[0m\n",atom[i].conductivity);
-			printf("\033[36m\tLUSTER : %s\033[0m\n",atom[i].luster);
-			printf("\033[36m\tSTATE OF MATTER : %s\033[0m\n",atom[i].phase);
-			printf("\033[36m\tDENSITY : %f\033[0m\n",atom[i].density);
-			printf("\033[36m\tUses : %s\033[0m\n",atom[i].uses);
-			break;
-		}
-	}
-	again();
-}
-
-void info4() {
-	newScreen();
-	float atwt;
-	printf("\033[32mENTER THE ELEMENT'S ATOMIC WEIGHT : \033[0m");
-	scanf("%f",&atwt);
-	for (i=0; i<118; i++) {
-		if (atom[i].atomicwt==atwt){
-			printf("\033[36m\tELEMENT : %s\033[0m\n",atom[i].name);
-			printf("\033[36m\tSYMBOL : %s\033[0m\n",atom[i].symbol);
-			printf("\033[36m\tATOMIC NUMBER : %d\033[0m\n",atom[i].atomicnum);
-			printf("\033[36m\tATOMIC WEIGHT : %f\033[0m\n",atom[i].atomicwt);
-			printf("\033[36m\tELECTRONIC CONFIGURATION : %s\033[0m\n",atom[i].electronic_configuration);
-			printf("\033[36m\tHISTORY : %s\033[0m\n",atom[i].history);
-			printf("\033[36m\tMELTING POINT : %f\033[0m\n",atom[i].melting_point);
-			printf("\033[36m\tBOILING POINT : %f\033[0m\n",atom[i].boiling_point);
-			printf("\033[36m\tIONIC RADIUS : %f\033[0m\n",atom[i].ionic_radius);
-			printf("\033[36m\tISOTOPES : %d\033[0m\n",atom[i].isotopes);
-			printf("\033[36m\tELECTRONEGATIVITY : %f\033[0m\n",atom[i].electronegativity);
-			printf("\033[36m\tELEMENT COLOUR : %s\033[0m\n",atom[i].color);
-			printf("\033[36m\tPOSITION : ELEMENT IS IN  %d GROUP and %d PERIOD\033[0m\n",atom[i].group,atom[i].period);
-			printf("\033[36m\tCONDUCTIVITY : %s\033[0m\n",atom[i].conductivity);
-			printf("\033[36m\tLUSTER : %s\033[0m\n",atom[i].luster);
-			printf("\033[36m\tSTATE OF MATTER : %s\033[0m\n",atom[i].phase);
-			printf("\033[36m\tDENSITY : %f\033[0m\n",atom[i].density);
-			printf("\033[36m\tUses : %s\033[0m\n",atom[i].uses);
-			break;
-		}
-	}
-	again();
-}
-
-void search() {
-	newScreen();
-	printf("\033[32mSEARCH BY :- \033[0m\n\n");
-	printf("\033[33m\t1 -> ELEMENT\033[0m\n");
-	printf("\033[33m\t2 -> SYMBOL\033[0m\n");
-	printf("\033[33m\t3 -> ATOMIC NUMBER\033[0m\n");
-	printf("\033[33m\t4 -> ATOMIC WEIGHT\033[0m\n\n\n\n");
-	int choice2;
-	printf("\033[32mYOUR CHOICE : \033[0m");
-	scanf("%d",&choice2);
-	if (choice2==1){
-		info1();
-	} else if (choice2==2){
-		info2();
-	} else if (choice2==3){
-		info3();
-	} else if (choice2==4){
-		info4();
-	} else {
-		search();
-	}
-}
-
-int main() {
+int main(){
 	newScreen();
 	printf("\033[37m1 -> EXPLORE\033[0m\n");
 	printf("\033[37m2 -> QUIT\033[0m\n\n\n\n");
 	int choice1;
 	printf("\033[37mYOUR CHOICE : \033[0m");
 	scanf("%d",&choice1);
-	table();
 	if (choice1==1){
 		search();
 	} else if (choice1==2){
